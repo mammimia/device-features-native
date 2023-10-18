2df8828f4978243fee2139c49b8f9321b5fc9cc8
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-10-18 08:54:23 +0300

    🔧 fix(ImagePicker.js): import useState from 'react' to use state in functional component 🔧 fix(ImagePicker.js): import Image, Text, and Colors from 'react-native' to use them in the component ✨ feat(ImagePicker.js): add image state and image preview functionality to display the selected image ✨ feat(ImagePicker.js): add styles for image preview container and image to improve UI appearance

--- components/ui/ImagePicker.js
+++ components/ui/ImagePicker.js
@@ -3,9 +3,12 @@ import {
   launchCameraAsync,
   useCameraPermissions
 } from 'expo-image-picker';
-import { Alert, Button, StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native';
+import { Colors } from '../../constants/colors';
 
 function ImagePicker() {
+  const [image, setImage] = useState(null);
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
 
@@ -36,16 +39,38 @@ function ImagePicker() {
       aspect: [16, 9],
       quality: 0.5
     });
+
+    setImage(image.assets[0].uri);
   }
 
   return (
     <View>
-      <View></View>
+      <View style={styles.imagePreview}>
+        {image ? (
+          <Image style={styles.image} source={{ uri: image }} />
+        ) : (
+          <Text>No image yet.</Text>
+        )}
+      </View>
       <Button title="Take Image" onPress={takeImageHandler} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+  }
+});
 
 export default ImagePicker;

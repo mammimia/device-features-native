1757a1cb2cfd49993e63a2bb7001ab22e52971cd
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-10-18 08:18:56 +0300

    🔧 chore(app.json): add expo-image-picker plugin to support camera functionality in the app 📦 feat(ImagePicker.js): create ImagePicker component to handle image selection from camera 📦 feat(package.json): add expo-image-picker as a dependency to enable camera functionality in the app

---
+++ components/ui/ImagePicker.js
@@ -0,0 +1,51 @@
+import { View, Text, StyleSheet, Button, Alert } from 'react-native';
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus
+} from 'expo-image-picker';
+
+function ImagePicker() {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const response = await requestPermission();
+      return response.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        'Insufficient permissions!',
+        'You need to grant camera permissions to use this app.',
+        [{ text: 'Okay' }]
+      );
+      return false;
+    }
+
+    return true;
+  }
+  async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) return;
+
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5
+    });
+  }
+
+  return (
+    <View>
+      <View></View>
+      <Button title="Take Image" onPress={takeImageHandler} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ImagePicker;

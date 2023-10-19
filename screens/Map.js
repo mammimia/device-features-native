485f9b0e6cc07289d667f46549f448f4a3479d48
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-10-19 19:30:23 +0300

    🔧 fix(Map.js): import Marker component from react-native-maps to be able to display a marker on the map ✨ feat(Map.js): add functionality to select a location on the map and display a marker at the selected location

--- screens/Map.js
+++ screens/Map.js
@@ -1,15 +1,37 @@
+import { useState } from 'react';
 import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+
+const mapRegion = {
+  latitude: 37.78,
+  longitude: -122.43,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421
+};
 
 function Map() {
-  const mapRegion = {
-    latitude: 37.78,
-    longitude: -122.43,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421
-  };
+  const [selectedLocation, setSelectedLocation] = useState();
 
-  return <MapView style={styles.map} initialRegion={mapRegion} />;
+  function selectLocationHandler(event) {
+    const coordinates = {
+      latitude: event.nativeEvent.coordinate.latitude,
+      longitude: event.nativeEvent.coordinate.longitude
+    };
+
+    setSelectedLocation(coordinates);
+  }
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={mapRegion}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker title="Picked Location" coordinate={selectedLocation} />
+      )}
+    </MapView>
+  );
 }
 
 const styles = StyleSheet.create({
